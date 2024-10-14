34c6b45f28a6e36a4fb6f398f86a1212b05c3aa3
Muhammad Aidil Mirza <144346363+aidlmrza@users.noreply.github.com>
2024-10-14 11:23:37 +0800

    Add files via upload

---
+++ KaryaSeniDigitalV3/src/main/java/KaryaSeni/CRUDKaryaSeni.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package KaryaSeni;
+
+/**
+ *
+ * @author maidi
+ */
+public interface CRUDKaryaSeni {
+    
+    public void createKaryaSeni();
+    
+    public void readKaryaSeni();
+    
+    public void updateKaryaSeni();
+    
+    public void deleteKaryaSeni();
+    
+}
